1e3f53e42b0e00840d10bac59fe5cbe8384d8193
Austin Phy <atphy42@gmail.com>
2020-06-09 20:35:55 -0500

    added basic js modules

---
+++ src/javascripts/helpers/data/messageData.js
@@ -0,0 +1,36 @@
+const messageData = [
+  {
+    id: 1,
+    user: 'User goes here',
+    timestamp: 'Timestamp goes here',
+    body: 'Message goes here',
+  },
+  {
+    id: 2,
+    user: 'User goes here',
+    timestamp: 'Timestamp goes here',
+    body: 'Message goes here',
+  },
+  {
+    id: 3,
+    user: 'User goes here',
+    timestamp: 'Timestamp goes here',
+    body: 'Message goes here',
+  },
+  {
+    id: 4,
+    user: 'User goes here',
+    timestamp: 'Timestamp goes here',
+    body: 'Message goes here',
+  },
+  {
+    id: 5,
+    user: 'User goes here',
+    timestamp: 'Timestamp goes here',
+    body: 'Message goes here',
+  },
+];
+
+const getMessageData = () => messageData;
+
+export default { getMessageData };
