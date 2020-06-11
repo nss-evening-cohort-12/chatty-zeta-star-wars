d943f928a96742d1044bffb3b430432b7bd8bff5
Austin Phy <atphy42@gmail.com>
2020-06-11 09:49:59 -0500

    tweaks

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,7 +3,7 @@ import displayMessages from './components/messageArea';
 import keyEvent from './components/inputArea';
 import clear from './components/clearBtn';
 import darkMode from './components/darkMode';
-import '../styles/main.scss'
+import '../styles/main.scss';
 
 const init = () => {
   displayMessages.displayMessages();
