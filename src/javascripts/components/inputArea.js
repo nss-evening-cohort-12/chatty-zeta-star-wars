5d083cd5b525eb1ba409f6ca44912fe267ae54d5
Joshua Medlen <joshua246@gmail.com>
2020-06-10 19:28:04 -0500

    finished add message function

--- src/javascripts/components/inputArea.js
+++ src/javascripts/components/inputArea.js
@@ -1,10 +1,15 @@
 import dom from '../helpers/utils';
+import user from '../helpers/data/userData';
+
+const users = user.getUserData();
 
 const pushMessage = (e) => {
   const newMessage = e.target.value;
   let domString = '';
 
   domString += `
+    <div>${users[0].name}</div>
+    <div>X</div>
     <div>${newMessage}</div>
     `;
   dom.printToDom('#messageArea', domString);
