4017503c8d8e5d2818ec088ed069a701f8607fe6
Matt Logan <mlogan5212@gmail.com>
2020-06-14 14:59:10 -0500

    added gif search

--- src/javascripts/components/gif.js
+++ src/javascripts/components/gif.js
@@ -1,19 +1,25 @@
-/* eslint-disable arrow-parens */
-// import utils from '../helpers/utils';
+import axios from 'axios';
+import utils from '../helpers/utils';
 
 
 const testSearch = () => {
   const searchTerm = document.querySelector('.gifSearch').value;
   const api = 'https://api.giphy.com/v1/gifs/search?';
-  const apiKey = 'api_key=qfI5Wo6vzOcyU8NhyesGf77c63gw29P3';
+  const apiKey = 'api_key=****PUT API KEY HERE****';
   const query = `&q=$${searchTerm}$limit=10`;
   const url = api + apiKey + query;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => console.error(data));
-  // console.error(searchTerm);
-  // document.querySelector('.gifSearch').value = '';
+  axios.get(url).then((results) => {
+    const image = results.data.data[0].images.downsized.url;
+    const domstring = `<img src="${image}">`;
+    utils.printToDom('#exampleFormControlTextarea1', domstring);
+  });
+  document.querySelector('.gifSearch').value = '';
+};
+
+const gifListen = () => {
+  $('body').on('click', '.pleasework', testSearch);
 };
 
 $('body').on('click', '.pleasework', testSearch);
+export default { gifListen };
