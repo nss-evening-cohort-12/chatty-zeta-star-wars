b7a8cd89656bd77755c7aa9d775433bf9655d0c2
Matt Logan <mlogan5212@gmail.com>
2020-06-14 00:58:56 -0500

    delete button only shows for selected user

--- src/javascripts/components/messageArea.js
+++ src/javascripts/components/messageArea.js
@@ -14,7 +14,8 @@ const messageLimit = () => {
 
 const displayMessages = () => {
   let domString = '<div id="addedMessage">';
-
+  const allow = document.querySelector('input[type = radio]:checked').value;
+  console.error(allow);
 
   for (let i = 0; i < messages.length; i += 1) {
     const cleanTime = moment(messages[i].timestamp).format('MMMM Do, h:mm a');
@@ -24,7 +25,13 @@ const displayMessages = () => {
                 <img class="userAvatar" src="${messages[i].avatar}">
                 <p class="userName mb-0 ${messages[i].userId}">${messages[i].userId}</p>
                 <h6 class="timestamp mb-0">${cleanTime}</h6>
-                <span class="delete mb-0"><i id="${messages[i].id}" class="delete fas fa-times-circle ${messages[i].userId}"></i></span>
+                <div class="deleteButton">`;
+    if (messages[i].userId === allow) {
+      domString += ` 
+                <span class="delete mb-0"><i id="${messages[i].id}" class="delete fas fa-times-circle ${messages[i].userId}"></i></span>`;
+    }
+    domString += `
+                </div>
                 </div>
                 <div class="messageBody">
                 <p class="message">${messages[i].body}</P>                    
@@ -35,5 +42,7 @@ const displayMessages = () => {
     messageLimit();
   }
 };
+
+$('body').on('click', '.custom-control-input', displayMessages);
 
 export default { displayMessages };
